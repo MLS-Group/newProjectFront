4998dec61e65e0ae18b9062d7c19d223b47a2eb6
齐恬 <1038394183@qq.com>
2018-10-17 14:29:34 +0800

    commit update

--- config/config.js
+++ config/config.js
@@ -7,4 +7,4 @@
 const serverIp = "192.168.1.139";
 const serverPort = "8080";
 const requestUrl = "http://" + serverIp + ":" + serverPort + "/";
-const requestJson = false;//是否访问json静态数据，当为 false时，会访问后台接口
+const requestJson = true;//是否访问json静态数据，当为 false时，会访问后台接口
