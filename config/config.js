1655190710b883be7a6aee26ea5e002ba5ccbb34
Mr.Liu <lzj470612639@outlook.com>
2018-10-23 13:30:21 +0800

    1

--- config/config.js
+++ config/config.js
@@ -4,7 +4,9 @@
  * @Author qitian
  */
 
-const serverIp = "192.168.1.139";
+// const serverIp = "192.168.1.139";
+const serverIp = "localhost";
+
 const serverPort = "8080";
 const requestUrl = "http://" + serverIp + ":" + serverPort + "/";
 const requestJson = false;//是否访问json静态数据，当为 false时，会访问后台接口
